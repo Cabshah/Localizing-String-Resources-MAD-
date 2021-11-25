1e20f19c13f248ffa2304e61fcf2a010258504b5
Cabshah <52652739+Cabshah@users.noreply.github.com>
2021-11-25 14:23:18 +0500

    Add files via upload

---
+++ LanguageChageApp/app/src/main/java/com/example/languagechageapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.languagechageapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,R.string.this_is_a_toast,Toast.LENGTH_LONG).show();
+            }
+
+            });
+
+    }
+}
